93e5ac65d733d6d2df0a29dc4a8274f810e467bc
Callum McCann <cmccann51@gmail.com>
2023-04-24 21:33:36 -0500

    fixing tests

--- crates/dbtonic/tests/dbt_project_tests.rs
+++ crates/dbtonic/tests/dbt_project_tests.rs
@@ -1,31 +1,27 @@
 use std::fs::File;
 use std::io::Write;
+use std::env;
 
+use tempfile::tempdir;
 use dbtonic::validation::dbt_project_operations::DbtProject;
 
-fn setup_dbt_project() {
-    let mut file = File::create("dbt_project.yml").expect("Failed to create dbt_project.yml");
+fn setup_dbt_project(temp_dir: &tempfile::TempDir) {
+    let dbt_project_file_path = temp_dir.path().join("dbt_project.yml");
+    let mut file = File::create(&dbt_project_file_path).expect("Failed to create dbt_project.yml");
     file.write_all(b"name: test_project\nversion: '1.0.0'\n")
         .expect("Failed to write to dbt_project.yml");
 }
 
-fn cleanup_dbt_project() {
-    std::fs::remove_file("dbt_project.yml").expect("Failed to delete dbt_project.yml");
-}
-
 #[test]
 fn test_validate() {
-    setup_dbt_project();
+    let temp_dir = tempdir().expect("Failed to create temporary directory");
+    setup_dbt_project(&temp_dir);
+    
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    env::set_current_dir(temp_dir.path()).expect("Failed to change to temporary directory");
 
     let dbt_project = DbtProject;
     dbt_project.validate();
-
-    cleanup_dbt_project();
-}
-
-#[test]
-#[should_panic(expected = "Hey friend, it looks like you're not in a dbt project right now.")]
-fn test_validate_no_dbt_project() {
-    let dbt_project = DbtProject;
-    dbt_project.validate();
+    
+    env::set_current_dir(current_dir).expect("Failed to change back to original directory");
 }
