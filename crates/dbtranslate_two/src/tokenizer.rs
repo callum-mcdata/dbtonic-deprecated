ea5f1f65a12bf785db16831e6857a54c0e52ce29
Callum McCann <cmccann51@gmail.com>
2023-04-29 16:30:54 -0500

    fixing trie implementation

--- crates/dbtranslate_two/src/tokenizer.rs
+++ crates/dbtranslate_two/src/tokenizer.rs
@@ -1,13 +1,18 @@
 use std::collections::{HashMap, HashSet};
 use either::Either;
-use crate::tokens::{Token, TokenType, single_tokens, keywords, commands, white_space};
+use crate::tokens::{Token, TokenType, single_tokens, keywords, comments, commands, white_space};
 
 
 /// This is the overall struct that contains all of the information about 
 /// tokenizing strings. 
 #[derive(Debug)]
 pub struct Tokenizer {
+    /// Token hashmaps
     single_tokens: HashMap<String, TokenType>,
+    keywords: HashMap<String, TokenType>,
+    white_space: HashMap<String, TokenType>,
+    comments: HashMap<String, Option<String>>,
+    /// Empty vectors
     bit_strings: Vec<Either<String, (String, String)>>,
     byte_strings: Vec<Either<String, (String, String)>>,
     hex_strings: Vec<Either<String, (String, String)>>,
@@ -15,7 +20,25 @@ pub struct Tokenizer {
     identifier_escapes: Vec<String>,
     quotes: Vec<Either<String, (String, String)>>,
     string_escapes: Vec<String>,
-    // ... add other fields
+    /// 
+    numeric_literals: HashMap<String, String>,
+    encode: Option<String>,
+    identifier_can_start_with_digit: bool,
+    /// State properties
+    sql: String,
+    size: usize,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
+    col: usize,
+    sql_comments: Vec<String>,
+    char: char,
+    end: bool,
+    peek: char,
+    prev_token_line: isize,
+    prev_token_comments: Vec<String>,
+    prev_token_type: Option<TokenType>,
 }
 
 /// These are the implementation methods that are required for the Tokenizer struct.
@@ -33,7 +56,12 @@ impl Tokenizer {
         // ... add other initializations
 
         Tokenizer {
+            /// Token hashmaps
             single_tokens: single_tokens(),
+            keywords: keywords(),
+            white_space: white_space(),
+            comments: comments(),
+            /// Empty vectors
             bit_strings,
             byte_strings,
             hex_strings,
@@ -42,6 +70,24 @@ impl Tokenizer {
             quotes,
             string_escapes,
             // ... add other field assignments
+            numeric_literals: HashMap::new(),
+            encode: None,
+            identifier_can_start_with_digit: false,
+            /// State management
+            sql: String::new(),
+            size: 0,
+            tokens: Vec::new(),
+            start: 0,
+            current: 0,
+            line: 1,
+            col: 1,
+            sql_comments: Vec::new(),
+            char: '\0',
+            end: false,
+            peek: '\0',
+            prev_token_line: -1,
+            prev_token_comments: Vec::new(),
+            prev_token_type: None,
         }
     }
 
--- crates/dbtranslate_two/src/trie.rs
+++ crates/dbtranslate_two/src/trie.rs
@@ -9,24 +9,75 @@ use std::fmt;
 /// Each node stores a character and a map of children nodes.
 
 #[derive(PartialEq, Eq, Clone, Serialize, Deserialize)]
-pub struct TrieNode {
-    pub children: HashMap<char, TrieNode>,
+pub struct Trie {
+    pub children: HashMap<char, Trie>,
     pub is_end_of_word: bool,
 }
 
-impl TrieNode {
+impl Trie {
     pub fn new() -> Self {
-        TrieNode {
+        Trie {
             children: HashMap::new(),
             is_end_of_word: false,
         }
     }
+
+    /// This function takes a slice of string references &[&str] as its argument 
+    /// and creates a new Trie from the given keywords. The function iterates 
+    /// over the keywords and constructs the trie using nested HashMaps.
+    pub fn from_keywords(keywords: &[&str]) -> Self {
+        let mut trie = Trie::new();
+
+        for key in keywords {
+            let mut current = &mut trie;
+    
+            for ch in key.chars() {
+                current = current.children.entry(ch).or_insert_with(Trie::new);
+            }
+            current.is_end_of_word = true;
+        }
+    
+        trie
+    }
+
+    /// This function takes a reference to a Trie and a string reference &str as 
+    /// its arguments. It checks if the key is in the trie and returns a tuple with
+    /// the TrieResult and a reference to the sub-trie where the search stopped.
+    /// LIFETIME USAGE:
+    /// By adding the lifetime specifier 'a to the function, we're giving a name 
+    /// to this relationship between the input and output lifetimes. Specifically, 
+    /// the syntax <'a> introduces a lifetime parameter called 'a. Then, 
+    /// by annotating the input reference trie with &'a Trie and the output 
+    /// reference &'a TrieNode, we're telling the compiler that both the input 
+    /// and output references share the same lifetime 'a.
+    pub fn search<'a>(&'a self, key: &str) -> (TrieResult, &'a Trie) {
+        if key.is_empty() {
+            return (TrieResult::NotFound, self);
+        }
+    
+        let mut current = self;
+    
+        for ch in key.chars() {
+            match current.children.get(&ch) {
+                None => return (TrieResult::NotFound, current),
+                Some(node) => current = node,
+            }
+        }
+    
+        if current.is_end_of_word {
+            (TrieResult::Found, current)
+        } else {
+            (TrieResult::Prefix, current)
+        }
+
+    }
+
 }
 
 /// This display implementation only shows the is_end_of_word property if it 
 /// is true, otherwise it leaves it out. We still need the bool in the data 
 /// structure though, even if we don't need to show it.
-impl fmt::Display for TrieNode {
+impl fmt::Display for Trie {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "children: {:?}", self.children)?;
         if self.is_end_of_word {
@@ -37,7 +88,7 @@ impl fmt::Display for TrieNode {
 }
 
 /// Same thing as above
-impl fmt::Debug for TrieNode {
+impl fmt::Debug for Trie {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "children: {:?}", self.children)?;
         if self.is_end_of_word {
@@ -48,27 +99,6 @@ impl fmt::Debug for TrieNode {
 }
 
 
-/// This alias is so we can refer to a TrieNode as a Trie.
-pub type Trie = TrieNode;
-
-/// This function takes a slice of string references &[&str] as its argument 
-/// and creates a new Trie from the given keywords. The function iterates 
-/// over the keywords and constructs the trie using nested HashMaps.
-pub fn new_trie(keywords: &[&str]) -> Trie {
-    let mut trie = Trie::new();
-
-    for key in keywords {
-        let mut current = &mut trie;
-
-        for ch in key.chars() {
-            current = current.children.entry(ch).or_insert_with(TrieNode::new);
-        }
-        current.is_end_of_word = true;
-    }
-
-    trie
-}
-
 /// This enum is introduced with three variants, NotFound, Prefix, and Found, 
 /// to represent the result of searching a key in the trie.
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
@@ -78,36 +108,6 @@ pub enum TrieResult {
     Found,
 }
 
-/// This function takes a reference to a Trie and a string reference &str as 
-/// its arguments. It checks if the key is in the trie and returns a tuple with
-/// the TrieResult and a reference to the sub-trie where the search stopped.
-/// LIFETIME USAGE:
-/// By adding the lifetime specifier 'a to the function, we're giving a name 
-/// to this relationship between the input and output lifetimes. Specifically, 
-/// the syntax <'a> introduces a lifetime parameter called 'a. Then, 
-/// by annotating the input reference trie with &'a Trie and the output 
-/// reference &'a TrieNode, we're telling the compiler that both the input 
-/// and output references share the same lifetime 'a.
-pub fn in_trie<'a>(trie: &'a Trie, key: &str) -> (TrieResult, &'a TrieNode) {
-    if key.is_empty() {
-        return (TrieResult::NotFound, trie);
-    }
-
-    let mut current = trie;
-
-    for ch in key.chars() {
-        match current.children.get(&ch) {
-            None => return (TrieResult::NotFound, current),
-            Some(node) => current = node,
-        }
-    }
-
-    if current.is_end_of_word {
-        (TrieResult::Found, current)
-    } else {
-        (TrieResult::Prefix, current)
-    }
-}
 
 #[cfg(test)]
 mod tests {
@@ -118,7 +118,7 @@ mod tests {
     #[test]
     fn test_new_trie() {
         let keywords = ["bla", "foo", "blab"];
-        let trie = new_trie(&keywords);
+        let trie = Trie::from_keywords(&keywords);
 
         assert_eq!(
             trie.children.get(&'b').unwrap()
@@ -144,20 +144,20 @@ mod tests {
         );
     }
 
-    /// This test checks if the in_trie function correctly identifies whether 
+    /// This test checks if the search function correctly identifies whether 
     /// a key is not found, is a prefix, or is found in the trie.
     #[test]
     fn test_in_trie() {
         let keywords = ["cat"];
-        let trie = new_trie(&keywords);
+        let trie = Trie::from_keywords(&keywords);
 
-        let (result1, _) = in_trie(&trie, "bob");
+        let (result1, _) = Trie::search(&trie, "bob");
         assert_eq!(result1, TrieResult::NotFound);
 
-        let (result2, _) = in_trie(&trie, "ca");
+        let (result2, _) = Trie::search(&trie, "ca");
         assert_eq!(result2, TrieResult::Prefix);
 
-        let (result3, _) = in_trie(&trie, "cat");
+        let (result3, _) = Trie::search(&trie, "cat");
         assert_eq!(result3, TrieResult::Found);
     }
 }
